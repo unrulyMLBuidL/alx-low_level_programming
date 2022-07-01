821d8cc2a511b4288e3a461a5b63d845027cce76
Opabunmi Micheal <opabunmi.micheal@gmail.com>
2022-07-01 08:54:46 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+/** main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char prog[] = "Programming is like building a multilingual puzzle";
+	char prog[] = "Programming is like building a multilingual puzzle\n";
 
 	puts(prog);
 	return (0);
