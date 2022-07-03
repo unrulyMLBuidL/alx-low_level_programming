d9ea46cd57f4c2d193be4a2fd5b6eaa34781bab4
Opabunmi Micheal <opabunmi.micheal@gmail.com>
2022-07-02 23:31:31 -0700

    corrections to task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,13 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 
-/** more headers goes there 
- *
- * main -entry point
- *
- * Return: Always 0 (Success)
- * betty style doc for function main goes there 
- */
+/* more headers goes there */
+
+
+
+/* betty style doc for function main goes there */
 int main(void)
 
 {
