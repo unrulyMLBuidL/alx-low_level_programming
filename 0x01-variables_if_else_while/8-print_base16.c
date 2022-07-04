333f2d627e556146476a2fb2ac3adf7473fedb95
Opabunmi Micheal <opabunmi.micheal@gmail.com>
2022-07-04 06:27:05 -0700

    8-printbase16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/** 
+/**
  * main - entry point.
  *
  * DES: print values of base 16 from 0 in low_case
@@ -21,5 +21,6 @@ int main(void)
 	putchar(a);
 	}
 	putchar('\n');
-	return 0;
+	return (0);
+
 }
