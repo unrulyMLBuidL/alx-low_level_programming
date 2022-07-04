53d70bb912a9e8e7149dee2fcb293b14db202266
Opabunmi Micheal <opabunmi.micheal@gmail.com>
2022-07-03 23:45:46 -0700

    7-print_tebhapla.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,18 +3,38 @@
 /**
  * main - entry point
  *
- * Description: Print all the single digit number of base 10 starting from 0. Only use putchar
+ * Description: Print all the single digit number of base 10 starting from 0
  *
  * Return: ALways 0 (Success)
  */
 int main(void)
 {
-	int a;
+	char a;
 
-	for (a = 0; a < 10; a++)
+	a = '';
+	for (; a < 10; )
 	{
 	putchar(a);
 	}
 	return (0);
+}
+
+
+
+
+
+
+
+
+/**
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+	putchar('int a');
+	}
+	return (0);
 
 }
+*/
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: printall the lowercase alphabet in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char z;
+
+	z = 'z';
+	for (; z >= 'a'; )
+	{
+	putchar(z);
+	z--;	
+	}
+	putchar('\n');
+	return (0);
+
+}
