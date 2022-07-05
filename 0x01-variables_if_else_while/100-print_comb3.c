381af8fa516c08a80a85bfc9d8f1f8148e24a0c9
Opabunmi Micheal <opabunmi.micheal@gmail.com>
2022-07-05 08:32:02 -0700

    0-putchar.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * DES: print all possible combinations of numbers with ','.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+		for (b = 0; b < 10; b++)
+		{
+			if (a,b != )
+			putchar(a,b + '0','0');
+		}
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point.
+ *
+ * Des: print _putchar.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+
+}
