7a8c2cc7dcc1338704ece805f297437c8d0577e8
Opabunmi Micheal <opabunmi.micheal@gmail.com>
2022-07-08 00:37:47 -0700

    3-integters

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -7,30 +7,23 @@
  * @c: third integer
  * Return: largest number
  */
-
 int largest_number(int a, int b, int c)
 {
     int largest;
 
     if (a >= b && a >=  c)
     {
-
 	largest = a;
-
     }
 
     else if (b >= c && b >= a)
     {
-
         largest = b;
-
     }
 
     else
     {
-
         largest = c;
-
     }
 
 
