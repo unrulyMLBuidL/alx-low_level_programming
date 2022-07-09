214b0839a5958f960d4ae67cc168a4648a226a88
Opabunmi Micheal <opabunmi.micheal@gmail.com>
2022-07-09 00:15:41 -0700

    2-alphabet.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times.
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		for (a = 'a'; a < 'z'; a++)
+			_putchar(a);
+		_putchar('\n');
+		i++;
+	}
+}
