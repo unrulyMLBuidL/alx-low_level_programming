c28a17d2a46d62cf8b9978fd0db39e74a7f134f6
Opabunmi Micheal <opabunmi.micheal@gmail.com>
2022-07-09 03:00:28 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs: Entry point.
+ * @i: variable
+ * _abs function - computes the absolute value of int.
+ * return: int value
+ */
+int _abs(int i)
+{
+	if (i > 0)
+	{
+		return (i);
+	}
+	
+	else if (i < 0)
+	{
+		return (-i);
+	}
+	
+	else
+	return (i);
+}
