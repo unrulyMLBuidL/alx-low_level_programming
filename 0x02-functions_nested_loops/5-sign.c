3de0707b03081836b681b85b0f2cb30bc79c3163
Opabunmi Micheal <opabunmi.micheal@gmail.com>
2022-07-09 01:52:57 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point.
+ * Description: Prints if an int is positive or negative.
+ * Return: 1 if positive, 0 if zero, -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
