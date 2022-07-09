7bf021f9a966da2b55d4be8c555a0cd6d42ed4de
Opabunmi Micheal <opabunmi.micheal@gmail.com>
2022-07-09 01:26:02 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * Description: checks for lowercase or uppercase character.
+ * @c: variable
+ * Return: int 1 if true or 0 if false
+ */
+int _isalpha(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+
+	{
+
+		return (1);
+
+	}
+	
+	if (c >= 'A' && c <= 'Z')
+
+	{
+		return (1);
+
+	}
+
+	else
+
+	{
+
+		return (0);
+
+	}
+}
