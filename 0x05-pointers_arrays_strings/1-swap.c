1ca718cee272954df13c3779eb20ae064246e9f4
Opabunmi Micheal <opabunmi.micheal@gmail.com>
2022-07-12 10:21:31 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,19 +4,15 @@
  * swap_int - entry point
  * @a: parameter variable
  * @b: parameter variable
- * @n: int variable
- * @p: int variable
+ * @n: varaible.
  * description: swaps the values of two integers
  * return: void
  */
 void swap_int(int *a, int *b)
 {
 	int n;
-	int p;
 
 	n = *a;
-	p = *b;
-
-	*a = p;
+	*a = *b;
 	*b = n;
 }
