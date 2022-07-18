32e6d400ced93471cdd9f4e732b049d8899dcb19
Opabunmi Micheal <opabunmi.micheal@gmail.com>
2022-07-18 06:37:43 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory of '*s' with a constant byte.
+ * @s: pointer to memory area s
+ * @b: constant byte of type char
+ * @n: number of byte to be allocated
+ * Return: char pointer to pointer 's'
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char **str = &s;
+	unsigned int i = 0;
+
+	while (i < n && n > 0)
+	{
+		s[i] = b;
+		i++;
+	}
+	return *str;
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * list of all the prototypes used in this working directory
+ */
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
